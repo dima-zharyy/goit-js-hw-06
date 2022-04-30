b1f9a678f4ac6e6196f5f6f0cf24c1dbc111ae9e
dima-zharyy <zharyydimka@gmail.com>
2022-04-30 17:16:11 +0300

    added-second-task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createLiElementsFromData = (array) => {
+  const newElements = array.map((el) => {
+    const newElement = document.createElement("li");
+    newElement.textContent = el;
+    newElement.classList.add("item");
+    return newElement;
+  });
+  return newElements;
+};
+
+const newElements = createLiElementsFromData(ingredients);
+const ingredientsListEl = document.querySelector("#ingredients");
+ingredientsListEl.append(...newElements);
